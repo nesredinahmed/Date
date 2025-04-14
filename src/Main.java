c251d5108ab2a929c42d7816f477afb10899c892
Nesredin Ahmed <ahmednesredin@icloud.com>
2025-04-14 13:16:33 -0700

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Date;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) { //ARRAY OF STRINGS
+        Date d = new Date(0);
+        char c = 'A'; // a single letter, number or symbol like a dollar sign or a space.
+        String fullName = "Kevin Ernest Long"; // String is a collection of character similar to an array.
+        String[] fruit = {"Apple", "Banana", "Cherry"}; // An Array ("list") or String
+
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(fullName);
+//        System.out.println(fruit[0] + fruit[1] + fruit[2] + fruit[3]);
+        System.out.println(fruit.length); //show length property
+        for(String f : fruit){
+            System.out.println(f);
         }
+//        for(int i = 0; i < fruit.length; i++) {  // i = i + 1
+//            //0, 1, 2, x
+//            System.out.println(i);
+//            System.out.println(fruit[i]);
+//        }
+
+        System.out.println("ALL DONE");
     }
 }
